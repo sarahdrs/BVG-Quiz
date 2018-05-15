a61fcb454a028d472981b535425291750e68bc1d
sarahdrs <37656248+sarahdrs@users.noreply.github.com>
2018-05-15 14:49:50 +0200

    Update bvg.js

--- bvg.js
+++ bvg.js
@@ -1,87 +1,92 @@
 var questions = [
   {question: "Turmstrasse",
   answers: [
-    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/2/24/Berlin_U8.svg' width=200px>"},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/e/e5/Berlin_U9.svg'width=200px>", isCorrect: true},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/9/91/Berlin_U2.svg'width=200px>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/2/24/Berlin_U8.svg' >"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/e/e5/Berlin_U9.svg'>", isCorrect: true},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/9/91/Berlin_U2.svg'>"},
   ]
 },
 {
   question: "Platz der Luftbrücke",
   answers: [
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/c/c7/Berlin_U55.svg'width=200px>"},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/2/25/Berlin_U6.svg'width=200px>", isCorrect: true},
-    {img:"<img src='https://upload.wikimedia.org/wikipedia/commons/e/e1/Berlin_U4.svg'width=200px>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/c/c7/Berlin_U55.svg'>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/2/25/Berlin_U6.svg'>", isCorrect: true},
+    {img:"<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/e/e1/Berlin_U4.svg'>"},
   ]
 },
 {
   question: "Heinrich-Heine-Strasse",
   answers: [
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/d/da/Berlin_U5.svg'width=200px>"},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/2/25/Berlin_U6.svg'width=200px>"},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/2/24/Berlin_U8.svg' width=200px>", isCorrect: true},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/d/da/Berlin_U5.svg'>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/2/25/Berlin_U6.svg'>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/2/24/Berlin_U8.svg' >", isCorrect: true},
   ]
 },
 {
   question: "Nauener Platz",
   answers: [
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/e/e5/Berlin_U9.svg'width=200px>", isCorrect: true},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/e/e1/Berlin_U4.svg'width=200px>"},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/d/da/Berlin_U5.svg'width=200px>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/e/e5/Berlin_U9.svg'>", isCorrect: true},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/e/e1/Berlin_U4.svg'>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/d/da/Berlin_U5.svg'>"},
   ]
 },
 {
   question: "Innsbrucker Platz",
   answers: [
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/2/25/Berlin_U6.svg'width=200px>"},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/e/e1/Berlin_U4.svg'width=200px>", isCorrect: true},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/c/c7/Berlin_U55.svg'width=200px>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/2/25/Berlin_U6.svg'>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/e/e1/Berlin_U4.svg'>", isCorrect: true},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/c/c7/Berlin_U55.svg'width=200px>"},
   ]
 },
 {
   question: "Wittenbergplatz",
   answers: [
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/9/9f/Berlin_U1.svg'width=200px>", isCorrect: true},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/2/24/Berlin_U8.svg' width=200px>"},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/d/da/Berlin_U5.svg'width=200px>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/9/9f/Berlin_U1.svg'>", isCorrect: true},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/2/24/Berlin_U8.svg' >"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/d/da/Berlin_U5.svg'>"},
   ]
 },
 {
   question: "Berliner Strasse",
   answers: [
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/9/9f/Berlin_U1.svg'width=200px>"},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/e/e1/Berlin_U4.svg'width=200px>"},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/e/e5/Berlin_U9.svg'width=200px>", isCorrect: true},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/9/9f/Berlin_U1.svg'width=>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/e/e1/Berlin_U4.svg'width=>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/e/e5/Berlin_U9.svg'width=>", isCorrect: true},
   ]
 },
 {
   question: "Freie Universität",
   answers: [
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/9/9b/Berlin_U3.svg'width=200px>", isCorrect: true},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/2/24/Berlin_U8.svg' width=200px>"},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/e/e5/Berlin_U9.svg'width=200px>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/9/9b/Berlin_U3.svg'width=>", isCorrect: true},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/2/24/Berlin_U8.svg' width=>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/e/e5/Berlin_U9.svg'width=>"},
   ]
 },
 {
   question: "Alexanderplatz",
   answers: [
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/2/24/Berlin_U8.svg' width=200px>", isCorrect: true},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Berlin_U7.svg'width=200px>"},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/9/9f/Berlin_U1.svg'width=200px>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/2/24/Berlin_U8.svg' width=>", isCorrect: true},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Berlin_U7.svg'width=>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/9/9f/Berlin_U1.svg'width=>"},
   ]
 },
 {
   question: "Rathaus Neukölln",
   answers: [
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/9/9b/Berlin_U3.svg'width=200px>"},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Berlin_U7.svg'width=200px>", isCorrect: true},
-    {img: "<img src='https://upload.wikimedia.org/wikipedia/commons/e/e1/Berlin_U4.svg'width=200px>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/9/9b/Berlin_U3.svg'width=>"},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Berlin_U7.svg'width=>", isCorrect: true},
+    {img: "<img class=answerimage src='https://upload.wikimedia.org/wikipedia/commons/e/e1/Berlin_U4.svg'width=>"},
   ]
 },
 ];
 var score = 0;
 var iQuestion = 0;
 
+
+function headerDisplay(){
+  document.getElementById("header").style.display="none";
+  console.log("hi")
+}
 
 
 /*function answerColor() {
@@ -96,17 +101,16 @@ var iQuestion = 0;
 
 function resultScore() {
   if (score>8) {
-    document.getElementById("result-score").innerHTML="<h1><u>Your Result:</u></h1><p>Awwweeeesssooommme! <br>When it comes to directions only to bravest will dare to questions your wisdom about the U-Bahn.</p><iframe src='https://giphy.com/gifs/d2Z9QYzA2aidiWn6/html5' width= '365' height='200'></iframe>";
+    document.getElementById("result-score").innerHTML="<h1><u>Your Result</u></h1><p>Awwweeeesssooommme! <br>BVG App, Google Maps? You don't need that shit because it's all in your head!</p><iframe src='https://giphy.com/gifs/d2Z9QYzA2aidiWn6/html5' width= '400' height='250'></iframe>";
   }
   else if(score <=8 && score >4){
-    document.getElementById("result-score").innerHTML="<h1><u>Your Result:</u></h1><p>Not bad, but could be better! <br>Get yourself a &quotUmweltkarte&quot and go practising! </p><iframe src='https://giphy.com/gifs/7TqE0ohlC9o2Z9eCBH/html5'width= '480' height='202'></iframe>";
+    document.getElementById("result-score").innerHTML="<h1><u>Your Result</u></h1><p>Not bad, but could be better! <br>Get yourself a &quotVBB-Umweltkarte&quot and go practising! </p><iframe src='https://giphy.com/gifs/JyjWw4ZLdSPE4/html5'width=500  height=250></iframe>";
   }
   else{
-    document.getElementById("result-score").innerHTML="<h1><u>Your Result:</u></h1><p>Seriously? Why did you even bother to try?</p><iframe src='https://giphy.com/gifs/rKj0oXtnMQNwY/html5'width= '500' height='371'></iframe>"
+    document.getElementById("result-score").innerHTML="<h1><u>Your Result</u></h1><p>Seriously? Why did you even bother to try?</p><iframe src='https://giphy.com/gifs/rKj0oXtnMQNwY/html5'width= '550' height='400'></iframe>"
   }
-
+document.getElementsByClassName('score')[0].innerHTML = score;
 }
-
 
 function updateQuestionAndScore() {
   if (iQuestion >= questions.length) {
@@ -116,7 +120,7 @@ function updateQuestionAndScore() {
     document.getElementsByClassName('answer')[0].outerHTML = '';
     resultScore();
     document.getElementById("instruction").style.display="none";
-
+    document.getElementById("try-again").style.display="inline";
     return;
   }
   document.getElementsByClassName('score')[0].innerHTML = score;
@@ -133,6 +137,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     answers[i].onclick = function() {
       if (questions[iQuestion].answers[i].isCorrect) {
         score++;
+        console.log("this is the score:"+ score)
       }
 
       iQuestion++;
